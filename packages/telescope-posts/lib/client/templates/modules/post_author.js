46b6cdad2e4177a066ef6667031708e9939800bb
quarbby <lynnette-ng@outlook.com>
2017-03-28 19:50:09 +0800

    Add karma at posts

--- packages/telescope-posts/lib/client/templates/modules/post_author.js
+++ packages/telescope-posts/lib/client/templates/modules/post_author.js
@@ -6,5 +6,13 @@ Template.post_author.helpers({
     } else {
       return this.author;
     }
+  },
+  karma: function() {    
+    var user = Meteor.users.findOne(this.userId);
+    if (user) {
+      return Math.round(100*user.telescope.karma)/100;
+    } else {
+      return 0;
+    }   
   }
 });
